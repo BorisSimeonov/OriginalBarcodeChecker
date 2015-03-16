185ba551709dc6f4d838bc5cbb276610fd6b3a80
Boris Bogdanov Simeonov <twistbobg@gmail.com>
2015-03-16 22:28:39 +0200

    First commit

---
+++ OriginalPerfumeChecker/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace OriginalPerfumeChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelInput = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Coral;
+            this.label1.Location = new System.Drawing.Point(181, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Perfume Checker";
+            // 
+            // labelInput
+            // 
+            this.labelInput.AutoSize = true;
+            this.labelInput.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelInput.ForeColor = System.Drawing.Color.Coral;
+            this.labelInput.Location = new System.Drawing.Point(12, 42);
+            this.labelInput.Name = "labelInput";
+            this.labelInput.Size = new System.Drawing.Size(117, 15);
+            this.labelInput.TabIndex = 1;
+            this.labelInput.Text = "Enter bar code here:";
+            // 
+            // tbInput
+            // 
+            this.tbInput.BackColor = System.Drawing.Color.Purple;
+            this.tbInput.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbInput.ForeColor = System.Drawing.Color.Coral;
+            this.tbInput.Location = new System.Drawing.Point(47, 60);
+            this.tbInput.MaxLength = 20;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(160, 26);
+            this.tbInput.TabIndex = 2;
+            this.tbInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Purple;
+            this.button1.Location = new System.Drawing.Point(213, 60);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 26);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button1_MouseClick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Coral;
+            this.label3.Location = new System.Drawing.Point(332, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Result:";
+            // 
+            // tbResult
+            // 
+            this.tbResult.BackColor = System.Drawing.Color.Purple;
+            this.tbResult.Font = new System.Drawing.Font("Times New Roman", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbResult.ForeColor = System.Drawing.Color.Coral;
+            this.tbResult.Location = new System.Drawing.Point(352, 60);
+            this.tbResult.MaxLength = 20;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(149, 26);
+            this.tbResult.TabIndex = 5;
+            this.tbResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbResult.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Purple;
+            this.ClientSize = new System.Drawing.Size(513, 112);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.labelInput);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaximumSize = new System.Drawing.Size(529, 150);
+            this.MinimumSize = new System.Drawing.Size(529, 150);
+            this.Name = "Form1";
+            this.Text = "Perfume Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelInput;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ OriginalPerfumeChecker/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OriginalPerfumeChecker
+{
+    public partial class Form1 : Form
+    {
+        string userInput = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseClick(object sender, MouseEventArgs e)
+        {
+            userInput = tbInput.Text;
+            bool check = checkInput();
+            if(check)
+            {
+                checkOriginality();
+            }
+        }
+
+        
+        private bool checkInput()
+        {
+            if (userInput.Length < 2)
+            {
+                return false;
+            }
+            else
+            {
+                char ch = ' ';
+                for (int cnt = 0; cnt < userInput.Length; cnt++)
+                {
+                    ch = userInput[cnt];
+                    if (!Char.IsDigit(ch))
+                    {
+                        tbInput.Text = "Invalid Input.";
+                        break;
+                    }
+                }
+                return true;
+            }
+        }
+
+        private void checkOriginality()
+        {
+            int controlNumber = (int)Char.GetNumericValue(userInput[0]);
+            int evenNumbers = 0;
+            int oddNumbers = 0;
+            int result = 0;
+            string finalConclusion = "";
+
+            for (int cnt = 1; cnt < userInput.Length; cnt++ )
+            {
+                if (cnt % 2 == 0)
+                {
+                    oddNumbers += (int)Char.GetNumericValue(userInput[cnt]);
+                }
+                else
+                {
+                    evenNumbers += (int)Char.GetNumericValue(userInput[cnt]);
+                }
+            }
+            evenNumbers *= 3;
+            result = 10 - ((oddNumbers + evenNumbers) % 10);
+            if(result == controlNumber)
+            {
+                finalConclusion = " Original!";
+            }
+            else
+            {
+                finalConclusion = " Fake!";
+            }
+
+            tbResult.Text = finalConclusion;
+        }
+    }
+}
